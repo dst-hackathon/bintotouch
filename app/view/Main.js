1a0abcc78d5f3d05debaf7f23739ff5d1a38c0c7
opaslai <opas.lai@gmail.com>
2013-08-16 16:58:06 +0700

    Add Random button

--- app/view/Main.js
+++ app/view/Main.js
@@ -10,38 +10,33 @@ Ext.define('BintoTouch.view.Main', {
 
         items: [
             {
-                title: 'Welcome',
+                title: 'Search',
                 iconCls: 'home',
 
                 styleHtmlContent: true,
                 scrollable: true,
 
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Binto'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Random'
+                    }
+                ]
             },
             {
-                title: 'Get Started',
+                title: 'Add',
                 iconCls: 'action',
 
                 items: [
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                        title: 'Add'
                     }
                 ]
             }
