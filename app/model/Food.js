5dcf0bb51ddbe69f7224c46cdfc1966f57b74d57
Oeam <sasitorn.oeam@gmail.com>
2013-08-17 17:40:41 +0700

    change model

--- app/model/Food.js
+++ app/model/Food.js
@@ -4,8 +4,9 @@ Ext.define('BintoTouch.model.Food', {
     config: {
         fields: [
             { name: 'id', type: 'int' },
-            { name: 'image_code', type: 'object'},
+            { name: 'image_code', type: 'string'},
             { name: 'name', type: 'string' },
+            { name: 'restaurant_name', type: 'string' },
             { name: 'price', type: 'string' }
         ]
     }
