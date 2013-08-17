64bfb030baf066af35a4482f2e2c58bd3bdeaf86
opal charoensit <smiley.opal@gmail.com>
2013-08-17 18:14:30 +0700

    Add text fields and button.

--- app/view/add/List.js
+++ app/view/add/List.js
@@ -5,17 +5,72 @@ Ext.define('BintoTouch.view.add.List', {
         title: 'Add',
         iconCls: 'add',
         iconMask: true,
-        items: [{
-            xtype: 'toolbar',
+        styleHtmlContent: true,
+        scrollable: true,
+        layout: {
+			type:'vbox',
+        	align: 'center'
+        },
+        items: [
+        {
             docked: 'top',
-            ui: 'light',
-            items: [{
-                xtype: 'searchfield',
-                placeHolder: 'Search by keywords...',
-                flex: 1
-            }]
-        }]
+            xtype: 'titlebar',
+            title: 'Binto'
+        },{
+				xtype: 'fieldset',
+				width: 620,
+				name: 'inputFields',
+				title: 'Add Information here....',
+				items:[
+	                	{
+	                    	xtype: 'textfield',
+							labelWidth: 120,
+	                    	name : 'restaurantName',
+	                    	label: 'Restaurant Name'
+	                	},
+	                	{
+	                    	xtype: 'textfield',
+							labelWidth: 120,
+	                    	name : 'foodName',
+	                    	label: 'Food Name'
+	                	},
+	                	{
+	                    	xtype: 'textfield',
+							labelWidth: 120,
+	                    	name : 'price',
+	                    	label: 'Price'
+	                	}
+				]
+		},
+		{
+			xtype: 'container',
+			layout:{
+				type: 'hbox',
+				align: 'center'
+			},
+			items:[
+						{
+							xtype: 'button',
+							text: 'Browse Image',
+							margin: '0 20'
+						},
+						{
+							xtype: 'button',
+							text: 'Upload Image',
+							margin: '0 20'
+						}
+			]
+		}
+		// {
+		//             xtype: 'toolbar',
+		//             docked: 'top',
+		//             ui: 'light',
+		//             items: [{
+		//                 xtype: 'searchfield',
+		//                 placeHolder: 'Search by keywords...',
+		// 		flex: 1
+		//             }]
+		//         },
+		]           
     }
-
-    
 });
